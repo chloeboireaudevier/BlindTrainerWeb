9594c55122c59eca72a9e9e5b63072b91f6acd55
chloeboireaudevier <cbd65@hotmail.fr>
2025-04-04 23:58:33 +0200

    edges and name of edges displayed (no randomized colors)

--- script.js
+++ script.js
@@ -4,12 +4,36 @@ let currentChar = '';
 function getRandomCorner() {
     const randomIndex = Math.floor(Math.random() * CONST.corners_key_list.length);
     const randomKey = CONST.corners_key_list[randomIndex];
-
     const value = CONST.corners.get(randomKey);
-
     return [randomKey,value]
 }
 
+function getRandomEdge() {
+    const randomIndex = Math.floor(Math.random() * CONST.edges_key_list.length);
+    const randomKey = CONST.edges_key_list[randomIndex];
+    const value = CONST.edges.get(randomKey);
+    return [randomKey,value]
+}
+
+function changeColors() {
+    // Get the square elements by ID
+    const square1 = document.getElementById('square1');
+    const square2 = document.getElementById('square2');
+
+    // Set new random colors for the squares
+    const res = getRandomEdge();
+    square1.style.backgroundColor = res[0][0];
+    square2.style.backgroundColor = res[0][1];
+    document.getElementById('char-display').textContent = res[1];
+
+}
+
+// Add event listener to the button
+document.getElementById('changeColorButton').addEventListener('click', changeColors);
+
+
+
+/*
 function startTraining() {
   nextChar();
   document.getElementById('feedback').textContent = '';
@@ -34,32 +58,4 @@ document.getElementById('input-box').addEventListener('input', function(e) {
     nextChar();
   }
 });
-
-// Function to generate random colors
-function getRandomColor() {
-    const randomIndex = Math.floor(Math.random() * CONST.edges_key_list.length);
-    const randomKey = CONST.edges_key_list[randomIndex];
-
-    return randomKey
-    const letters = '0123456789ABCDEF';
-    let color = '#';
-    for (let i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-}
-
-// Function to change the colors of the squares
-function changeColors() {
-    // Get the square elements by ID
-    const square1 = document.getElementById('square1');
-    const square2 = document.getElementById('square2');
-
-    // Set new random colors for the squares
-    const newcolors = getRandomColor();
-    square1.style.backgroundColor = newcolors[0];
-    square2.style.backgroundColor = newcolors[1];
-}
-
-// Add event listener to the button
-document.getElementById('changeColorButton').addEventListener('click', changeColors);
+*/
